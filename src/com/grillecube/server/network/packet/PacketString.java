51bc076afadcf04d5b3672eaae241c141ded802a
U-Romain-HP\Romain <rpereira@student.42.fr>
2015-07-27 22:14:04 +0200

    added some explanations in PacketStrng.java

--- src/com/grillecube/server/network/packet/PacketString.java
+++ src/com/grillecube/server/network/packet/PacketString.java
@@ -5,6 +5,29 @@ import com.grillecube.common.network.Packets;
 
 import io.netty.buffer.ByteBuf;
 
+/**
+ * 
+ * Simple exception of a packet definition:
+ *
+ *	• WHEN A PACKET IS SENT:
+ *	e.g:	new PacketString("Hello world").send(channel);	<- will send the packet to the given channel
+ *	'Packet.send()' will create a new ByteBuf of size '4 + Packet.getPacketSize()'
+ *	the 4 first bytes is an int that contains the packet ID ('Packet.getPacketID')
+ *	'Packet.getPacketSize()' correspond to the number of bytes written in 'Packet.writeData()' function
+ *	When this byteBuffer is created, it is filled by calling 'Packet.writeData()' function implementation
+ *
+ *	• WHEN A PACKET IS RECEIVED:
+ *	'Packet.fromByteBuffer(ByteBuf buff)' will return a new instance of this Packet, which contains the received data
+ *	the 'PacketString(ByteBuf buff)' has to be implemented so when the packet is received,
+ *	java can call this constructor. Else way an exception is called on initialiaztion
+ *
+ *	When this constructor is called, the 'readData()' function is called,
+ *	so the packet attributes can be set
+ *	
+ *	e.g:	new PacketString(byteBuffer).getString(); <- will return the String attribute
+ */
+
+
 public class PacketString extends Packet
 {
 	private String	_str;
