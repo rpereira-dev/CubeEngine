202b66d0fb1eaa4e6fc7fc465fec4c9e03f5bd5d
U-Romain-HP\Romain <rpereira@student.42.fr>
2015-07-29 01:26:48 +0200

    editing ModBidon.java + removing useless skyboxes textures + removing bidon.jar

--- src/com/grillecube/client/mod/ModBidon.java
+++ src/com/grillecube/client/mod/ModBidon.java
@@ -2,23 +2,27 @@ package com.grillecube.client.mod;
 
 import com.grillecube.client.Game;
 import com.grillecube.common.mod.IMod;
+import com.grillecube.common.mod.ModInfo;
+
+import fr.toss.lib.Logger;
 
 /**
  * Modding test class
  */
+@ModInfo(author = "toss")
 public class ModBidon implements IMod
 {
-
 	@Override
 	public void initialize(Game game)
 	{
+		game.getLogger().log(Logger.Level.DEBUG, "Loading Bidon mods!");
 		game.getResourceManager().addRessource(new ResourceBlocks());
 	}
 
 	@Override
 	public void deinitialize(Game game)
 	{
-		// TODO Auto-generated method stub
+		game.getLogger().log(Logger.Level.DEBUG, "Unloading Bidon mods!");
 	}
 
 }
