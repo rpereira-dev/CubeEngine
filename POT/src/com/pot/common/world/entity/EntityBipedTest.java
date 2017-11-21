be8744ce3c330fd4001a626c8a29c670c1a52c97
rpereira <cedquen@hotmail.fr>
2017-11-21 21:44:59 +0100

    size offset

--- POT/src/com/pot/common/world/entity/EntityBipedTest.java
+++ POT/src/com/pot/common/world/entity/EntityBipedTest.java
@@ -7,9 +7,9 @@ public class EntityBipedTest extends EntityBiped {
 
 	public EntityBipedTest(World world) {
 		super(world);
-		super.setSizeX(1.0f);
-		super.setSizeY(2.0f);
-		super.setSizeZ(1.0f);
+		super.setSizeX(0.98f);
+		super.setSizeY(1.96f);
+		super.setSizeZ(0.98f);
 		super.setMass(100);
 	}
 
